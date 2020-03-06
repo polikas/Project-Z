010d368e93171004bfc3505d34de7433052f796a
Stavros Charitos <polikasxaritos@gmail.com>
2020-03-06 19:07:50 +0000

    Installed LRP package, add player, add ground, add movement system horizontal, add jump system, camera lock to player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpSystem();
+        MovementSystem();
+        
+    }
+
+    void JumpSystem()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+
+    void MovementSystem()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
